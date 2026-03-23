60499707e24801e2db9f8ef0cb869119d957c53b
ShameerHere <265220365+ShameerHere@users.noreply.github.com>
2026-03-23 22:02:00 +0000

    Finish rock paper scissors game Finished working on the rock, paper, scissors game. For future reference use math.floor rather than decimals. Avoid using return needlessly.

--- game.js
+++ game.js
@@ -11,33 +11,106 @@ function getComputerChoice(){
 
 
 function getHumanChoice(){
-   answer=prompt("rock,paper or scissors?")
+   let answer=prompt("rock,paper or scissors?")
    return answer.toLowerCase()
 }
 
-let human=getHumanChoice()
-let computer=getComputerChoice()
+function playGame(){   
+ let humanScore=0
+ let computerScore=0
+ playRound(getHumanChoice(), getComputerChoice())
+ playRound(getHumanChoice(), getComputerChoice())
+ playRound(getHumanChoice(), getComputerChoice())
+ playRound(getHumanChoice(), getComputerChoice())
+ playRound(getHumanChoice(), getComputerChoice())
 
-let humanScore=0
-let computerScore=0
-/* All you need to do now to finish the single round section is add a scoring
-system so computer wins they get a point, human wins they get a point. After
-that just move onto making it 5 rounds*/
-function playRound(humanChoice,computerChoice){
+if (computerScore>humanScore){
+    console.log("Computer Wins the Game!")
+    } else if (humanScore>computerScore){
+        console.log("You Win the Game!")
+    }else console.log ("Looks Like a Tie")
+ function playRound(humanChoice,computerChoice){
      if(humanChoice==="rock" && computerChoice==="paper"){
-        return humanScore++, console.log("You lose! Paper beats Rock")
+        return computerScore++, console.log("You lose! Paper beats Rock")
     } else if(humanChoice==="scissors" && computerChoice==="rock"){
-        return console.log("You lose! Rock beats Scissors")
+        return computerScore++,console.log("You lose! Rock beats Scissors")
     } else if(humanChoice==="paper" && computerChoice==="scissors"){
-        return console.log("You lose! scissors beats paper")
+        return computerScore++,console.log("You lose! scissors beats paper")
     } else if(humanChoice==="paper" && computerChoice==="rock"){
-        return console.log("You win! Paper beats Rock")
+        return humanScore++,console.log("You win! Paper beats Rock")
     } else if(humanChoice==="rock" && computerChoice==="scissors"){
-        return console.log("You win! Rock beats scissors")
+        return humanScore++,console.log("You win! Rock beats scissors")
     } else if(humanChoice==="scissors" && computerChoice==="paper"){
-        return console.log("You win! Scissors beats Paper")}
-     else return console.log("Draw!")
+        return humanScore++,console.log("You win! Scissors beats Paper")}
+     else return console.log("Draw!");
     }
+   
 
-function playGame
-playRound(human,computer)
+} 
+
+playGame()
+
+/*
+
+Just pasting my pseudocode here because i did it in another doc:
+
+2: A value from one of “rock”, “paper” or “scissors” is selected
+
+rock and paper and scissors-create 3 variables
+
+Then-create a function which chooses between them randomly
+
+0-0.33333
+
+3:
+
+Allow the user to input one of the three options
+
+prompt
+
+5:
+
+Take the human and computer players choices:
+
+humanChoice is the answer in response to the function getHumanChoice
+
+computerChoice is the result of the function getComputerChoice
+
+allow humanChoice to be case-insensitive
+
+so convert whatever the players said to lowercase
+
+convert answer to lowercase
+
+Play a round
+
+make rock beat scissors, scissors beat paper, paper beat rock,
+
+if same make it a draw
+
+Increase the winner’s score
+
+Announce a winner
+
+```jsx
+   return "You lose! Paper beats Rock"
+    
+    return "You lose! Rock beats Scissors"
+    
+    return "You lose! Scissors beats Paper"
+
+    return "You win! Paper beats Rock"
+    
+    return "You win! Rock beats Scissors"
+    
+    return "You win! Scissors beats Paper"
+    
+```
+
+Playing rock paper scissors-check
+
+playing it 5 times-break it down what if i just wanted to play it twice-check
+
+keeping track of the score-
+
+when someone gets 5 declare a winner*/
